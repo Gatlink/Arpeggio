c269b0d62dc9489ecf0250cd17bc8b83bad89d73
Gatlink <tyganit@gmail.com>
2015-08-06 09:22:30 +0200

    Improve further the pulse code.

--- Assets/Scipts/Pulse.cs
+++ Assets/Scipts/Pulse.cs
@@ -9,13 +9,13 @@ public class Pulse : MonoBehaviour
     public float MaxScale = 1.5f;
 
     private GameObject _pulse;
+    private Image _pulseImage;
     private bool _startPulsing = true;
-    private Sprite _sprite;
 
     private void Start()
     {
         _pulse = transform.GetChild(0).gameObject;
-        _sprite = GetComponent<Image>().sprite;
+        _pulseImage = _pulse.GetComponent<Image>();
     }
 
     private void Update()
@@ -28,8 +28,7 @@ public class Pulse : MonoBehaviour
     {
         _startPulsing = false;
         var startTime = Time.time;
-        var renderer = _pulse.GetComponent<Image>();
-        var color = renderer.color;
+        var color = _pulseImage.color;
 
         while (Time.time - startTime <= Duration)
         {
@@ -37,13 +36,13 @@ public class Pulse : MonoBehaviour
             var scale = Mathf.Lerp(1, MaxScale, t);
             color.a = Mathf.Lerp(1, 0, t);
             _pulse.transform.localScale = new Vector3(scale, scale, 1);
-            renderer.color = color;
+            _pulseImage.color = color;
             yield return null;
         }
 
         _pulse.transform.localScale = Vector3.one;
         color.a = 1;
-        renderer.color = color;
+        _pulseImage.color = color;
         yield return new WaitForSeconds(TimeBetweenPulsations);
         _startPulsing = true;
     }
