fd47dad60285bb3c3df13fbd4186ce032f438fc5
Gatlink <tyganit@gmail.com>
2015-09-04 14:35:46 +0200

    Commands disappear when reaching the top of the screen.

--- Assets/Scripts/Command.cs
+++ Assets/Scripts/Command.cs
@@ -1,8 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 [RequireComponent(typeof(FadeOut))]
 public class Command : MonoBehaviour
 {
+    public string Axis;
+
     private FadeOut _fadeOut;
 
     public void Start()
@@ -13,10 +16,25 @@ public class Command : MonoBehaviour
     public void Update()
     {
 
-        if (Input.GetAxis("Command A") > 0)
-            _fadeOut.enabled = true;
+        if (Input.GetAxis(Axis) > 0)
+        {
+            Kill();
+        }
+    }
 
-        if (_fadeOut.Faded)
-            Destroy(gameObject);
+    public void Kill()
+    {
+        StartCoroutine(StartDying());
+    }
+
+    private IEnumerator StartDying()
+    {
+        _fadeOut.enabled = true;
+        enabled = false;
+
+        while (!_fadeOut.Faded)
+            yield return null;
+
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/SpawnCommands.cs
+++ Assets/Scripts/SpawnCommands.cs
@@ -3,13 +3,29 @@
 public class SpawnCommands : MonoBehaviour
 {
     public GameObject[] CommandPrefabs;
+    public float DeadZoneRatio = 0.8f;
 
     private Vector3 _spawnPosition;
+    private Command _commands;
+    private float _maxCommandY;
 
     public void Start()
     {
         _spawnPosition = transform.GetChild(0).position;
         var newInstance = (GameObject) Instantiate(CommandPrefabs[0], _spawnPosition, Quaternion.identity);
         newInstance.transform.SetParent(transform);
+        _commands = newInstance.GetComponent<Command>();
+
+        var rect = GetComponent<RectTransform>().rect;
+        _maxCommandY = rect.height * DeadZoneRatio + rect.yMin;
+    }
+
+    public void Update()
+    {
+        if (_commands && _commands.GetComponent<RectTransform>().anchoredPosition.y >= _maxCommandY)
+        {
+            _commands.Kill();
+            _commands = null;
+        }
     }
 }
